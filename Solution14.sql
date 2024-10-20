4ba29261ad20786c2ca29eb9e1b121f8c9d4e666
wriml92 <wriml92@knou.ac.kr>
2024-10-20 18:51:46 +0900

    add Solution14.sql

---
+++ Solution14.sql
@@ -0,0 +1,6 @@
+SELECT max(PRICE) MAX_PRICE
+from PRODUCT
+
+-- 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/131697
+-- 가장 높은 판매가를 구해야 하므로 max(PRICE) 사용
+-- MAX_PRICE 앞에 as가 생략되어 있음
