97d0914b4b77dc370f4d30d320c1316589b457a2
wriml92 <wriml92@knou.ac.kr>
2024-10-20 18:39:10 +0900

    add Solution10.sql

--- Solution9.sql
+++ Solution9.sql
@@ -1,5 +1,5 @@
 SELECT MIN(DATETIME) '시간'
 from ANIMAL_INS
 
-# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/59038
-# 가장 먼저 들어온 동물을 구해야 하므로 min 함수 사용
+-- 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/59038
+-- 가장 먼저 들어온 동물을 구해야 하므로 min 함수 사용
