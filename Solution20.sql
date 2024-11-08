6b1fb2541dad6d2d203e531d4017b98914948e4a
wriml92 <wriml92@knou.ac.kr>
2024-11-08 15:40:32 +0900

    limit 1

---
+++ Solution20.sql
@@ -0,0 +1,6 @@
+SELECT *
+from FOOD_PRODUCT
+order by PRICE desc
+limit 1
+-- 가격이 제일 비싼 식품의 정보 출력하기 (https://school.programmers.co.kr/learn/courses/30/lessons/131115)
+-- 가격이 제일 비싼 식품 행만 조회해 달라 했으므로 limit 1 추가
