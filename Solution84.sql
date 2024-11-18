066e5b3610fe4ad445d3d803ef3cf429a77be85e
wriml92 <wriml92@knou.ac.kr>
2024-11-18 12:08:50 +0900

    left join and count

---
+++ Solution84.sql
@@ -0,0 +1,7 @@
+select customer_id, count(1) count_no_trans
+from Visits v left join Transactions t on v.visit_id = t.visit_id
+where t.visit_id is null
+group by customer_id
+
+-- Customer Who Visited but Did Not Make Any Transactions (https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/)
+-- find the IDs of the users who visited without making any transactions and the number of times they made these types of visits
