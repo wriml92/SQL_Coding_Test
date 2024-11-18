6fb33444bab9658e2c1ffe07203c69743b03cff8
wriml92 <wriml92@knou.ac.kr>
2024-11-18 12:22:56 +0900

    inner join on

--- Solution84.sql
+++ Solution84.sql
@@ -3,5 +3,5 @@ from Visits v left join Transactions t on v.visit_id = t.visit_id
 where t.visit_id is null
 group by customer_id
 
--- Customer Who Visited but Did Not Make Any Transactions (https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/)
+-- Customer Who Visited but Did Not Make Any Transactions (https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions)
 -- find the IDs of the users who visited without making any transactions and the number of times they made these types of visits
